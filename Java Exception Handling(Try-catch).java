0da0832dec117848563549c487bcd5d8d1dc15f1
divyas19 <73398403+divyas19@users.noreply.github.com>
2022-04-13 15:55:44 +0530

    Create Java Exception Handling(Try-catch).java

---
+++ Java Exception Handling(Try-catch).java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        try
+        {
+          int a = sc.nextInt();
+          int b = sc.nextInt();
+          System.out.println(a/b);
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("java.lang.ArithmeticException: / by zero");
+        }
+        catch(InputMismatchException f)
+        {
+            System.out.println("java.util.InputMismatchException");
+        }
+      
+        sc.close();
+    }
+}
